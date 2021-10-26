c8803b799f8c6d3b07f674abf3295c35f3748de8
Mirraim <mirraim@gmail.com>
2021-10-26 11:33:38 +0500

    home page

---
+++ src/test/java/ru/mirraim/tacos/controller/HomeControllerTest.java
@@ -0,0 +1,40 @@
+package ru.mirraim.tacos.controller;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * Starts with the MockMvc object to perform an HTTP GET request for / (the root path)
+     * The response should have an HTTP 200 (OK) status.
+     * The view should have a logical name of home.
+     * The rendered view should contain the text “Welcome to….”
+     * If, after the MockMvc object performs the request, any of those expectations aren’t met,
+     * then the test fails
+     */
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        containsString("Welcome to...")));
+    }
+
+}

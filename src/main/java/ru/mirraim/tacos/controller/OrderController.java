8cd09ce8621d6b132dedb1e236368c5492caecdd
Mirraim <mirraim@gmail.com>
2021-10-28 17:13:52 +0500

    design taco and create order

---
+++ src/main/java/ru/mirraim/tacos/controller/OrderController.java
@@ -0,0 +1,27 @@
+package ru.mirraim.tacos.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.mirraim.tacos.entity.TacoOrder;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("tacoOrder", new TacoOrder());
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order) {
+        log.info("Order submitted: " + order);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/ru/mirraim/tacos/entity/Ingredient.java
@@ -0,0 +1,13 @@
+package ru.mirraim.tacos.entity;
+import lombok.Data;
+
+@Data
+public class Ingredient {
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}

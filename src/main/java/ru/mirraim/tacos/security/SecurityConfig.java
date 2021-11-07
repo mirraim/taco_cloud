31bab1bf44fc74546fbda67fcbe6a4379841b439
Mirraim <mirraim@gmail.com>
2021-11-07 21:16:08 +0500

    Securing requests

--- src/main/java/ru/mirraim/tacos/security/SecurityConfig.java
+++ src/main/java/ru/mirraim/tacos/security/SecurityConfig.java
@@ -2,10 +2,12 @@ package ru.mirraim.tacos.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import ru.mirraim.tacos.data.UserRepository;
 import ru.mirraim.tacos.entity.User;
 
@@ -23,5 +25,15 @@ public class SecurityConfig {
             if (user != null) return user;
             throw new UsernameNotFoundException("User '" + username + "' not found");
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests()
+                .antMatchers("/design", "/orders").hasRole("USER")
+                .antMatchers("/", "/**").permitAll()
+                .and()
+                .build();
     }
 }
